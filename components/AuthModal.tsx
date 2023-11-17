df0ea1c24037e5d6aa0a1b952ee74cf607bc0488
Pete Franco <petethe4th@live.com>
2023-11-16 22:45:45 -0600

    fixed p

--- components/AuthModal.tsx
+++ components/AuthModal.tsx
@@ -3,8 +3,8 @@
 import React, { useEffect } from 'react';
 import { Auth } from '@supabase/auth-ui-react';
 import { ThemeSupa } from '@supabase/auth-ui-shared';
-import { 
-  useSessionContext, 
+import {
+  useSessionContext,
   useSupabaseClient
 } from '@supabase/auth-helpers-react';
 import { useRouter } from 'next/navigation';
@@ -17,7 +17,7 @@ const AuthModal = () => {
   const { session } = useSessionContext();
   const router = useRouter();
   const { onClose, isOpen } = useAuthModal();
-  
+
   const supabaseClient = useSupabaseClient();
 
   useEffect(() => {
@@ -34,15 +34,15 @@ const AuthModal = () => {
   }
 
   return (
-    <Modal 
-      title="Welcome back" 
-      description="Login to your account." 
-      isOpen={isOpen} 
-      onChange={onChange} 
+    <Modal
+      title="Welcome back"
+      description="Login to your account."
+      isOpen={isOpen}
+      onChange={onChange}
     >
       <Auth
         supabaseClient={supabaseClient}
-        providers={['github']}
+        providers={[]}
         magicLink={true}
         appearance={{
           theme: ThemeSupa,
--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { FaPlay } from "react-icons/fa";
 
-import useAuthModal from "@/hooks/useAuthModal";
-import { useUser } from "@/hooks/useUser";
+// import useAuthModal from "@/hooks/useAuthModal";
+// import { useUser } from "@/hooks/useUser";
 
 interface ListItemProps {
   image: string;
@@ -19,32 +19,32 @@ const ListItem: React.FC<ListItemProps> = ({
   href,
 }) => {
   const router = useRouter();
-  const authModal = useAuthModal();
-  const { user } = useUser();
-  
+  // const authModal = useAuthModal();
+  // const { user } = useUser();
+
   const onClick = () => {
-    if (!user) {
-      return authModal.onOpen();
-    }
+    // if (!user) {
+    //   return authModal.onOpen();
+    // }
 
     router.push(href);
   };
 
-  return ( 
+  return (
     <button
       onClick={onClick}
       className="
-        relative 
-        group 
-        flex 
-        items-center 
-        rounded-md 
-        overflow-hidden 
-        gap-x-4 
-        bg-neutral-100/10 
-        cursor-pointer 
-        hover:bg-neutral-100/20 
-        transition 
+        relative
+        group
+        flex
+        items-center
+        rounded-md
+        overflow-hidden
+        gap-x-4
+        bg-neutral-100/10
+        cursor-pointer
+        hover:bg-neutral-100/20
+        transition
         pr-4
       "
     >
@@ -59,20 +59,20 @@ const ListItem: React.FC<ListItemProps> = ({
       <p className="font-medium truncate py-5">
         {name}
       </p>
-      <div 
+      <div
         className="
-          absolute 
-          transition 
-          opacity-0 
-          rounded-full 
-          flex 
-          items-center 
-          justify-center 
-          bg-green-500 
-          p-4 
-          drop-shadow-md 
+          absolute
+          transition
+          opacity-0
+          rounded-full
+          flex
+          items-center
+          justify-center
+          bg-green-500
+          p-4
+          drop-shadow-md
           right-5
-          group-hover:opacity-100 
+          group-hover:opacity-100
           hover:scale-110
         "
       >
@@ -81,5 +81,5 @@ const ListItem: React.FC<ListItemProps> = ({
     </button>
    );
 }
- 
+
 export default ListItem;
